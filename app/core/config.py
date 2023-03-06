00ea8a4d16471fd10cfb9360fb106e82b593eaa9
juanMaAV92 <juanmanuel.armero@gmail.com>
2023-03-05 21:47:52 -0500

    ⚙️ environment variables with pydantic[BaseSettings]

---
+++ app/core/config.py
@@ -0,0 +1,65 @@
+
+import os
+from functools import lru_cache
+
+from typing import Any, Dict, Optional
+from pydantic import BaseSettings, EmailStr, PostgresDsn, validator
+import pydantic
+
+
+class Settings( BaseSettings ):
+
+    API_V_STR :  str
+    API_RELOAD : bool
+
+    SERVER_NAME : str
+    SERVER_HOST : str
+    SERVER_PORT : int
+
+    PROJECT_NAME : str
+    PROJECT_DESCRIPTION : str
+    PROJECT_VERSION : str
+    PROJECT_DOCS_URL : str
+    PROJECT_REDOC_URL : str
+
+    POSTGRES_USER : str
+    POSTGRES_PASSWORD : str
+    POSTGRES_SERVER : str
+    POSTGRES_PORT : int
+    POSTGRES_DB : str
+    SQLALCHEMY_DATABASE_URI : Optional[ PostgresDsn ]
+
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[ str ], values: Dict[ str, Any] ) -> Any:
+        if isinstance( v, str ):
+            return v
+
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER" ),
+            password=values.get( "POSTGRES_PASSWORD" ),
+            host=values.get( "POSTGRES_SERVER" ),
+            port=str(values.get( 'POSTGRES_PORT' )),
+            path=f"/{values.get( 'POSTGRES_DB' ) or '' }",
+        )
+    
+    FIRST_SUPERUSER : EmailStr
+    FIRST_SUPERUSER_PASSWORD : str
+    USERS_OPEN_REGISTRATION : bool = False
+
+    class Config :
+        try:
+            env_file = f"{os.path.dirname(os.path.abspath(__file__))}/../.env"
+            env_file_encoding = 'utf-8'
+            case_sensitive = True
+        except:
+            env_file = '.env_dev'
+            env_file_encoding = 'utf-8'
+            case_sensitive = True
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
+
+
+settings = get_settings()
---
+++ app/main.py
@@ -0,0 +1,36 @@
+
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from core.config import settings
+
+
+app = FastAPI(
+    title = settings.PROJECT_NAME, 
+    description = settings.PROJECT_DESCRIPTION,
+    version = settings.PROJECT_VERSION,
+
+    docs_url = settings.PROJECT_DOCS_URL,
+    redoc_url = settings.PROJECT_REDOC_URL,    
+    openapi_url = f"{settings.API_V_STR}/openapi.json"
+)
+
+
+@app.get( f'/health', tags=[ 'health' ] )
+async def health():
+    return JSONResponse(    status_code = 200,
+                            content = [] )
+
+
+def run():
+    uvicorn.run(    'main:app',
+                    host = settings.SERVER_HOST,
+                    port = settings.SERVER_PORT,
+                    reload = settings.API_RELOAD,
+                    log_level = 'debug')
+
+
+if __name__ == '__main__':
+    run()
